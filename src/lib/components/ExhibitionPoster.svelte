95be63c2d3af43892278fbe16de54bf92dc9c43e
Jinsu Eun <eunjs0701@hanmail.net>
2024-06-04 17:37:47 +0900

    update exhibition

---
+++ src/lib/components/ExhibitionPoster.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let width = 0;
+    $: height = (width * 1684) / 1191;
+    $: qrX = width * 0.05;
+    $: qrY = width * 0.233;
+    $: qrW = width * 0.045;
+    $: qrH = width * 0.045;
+    let opacity = 0;
+</script>
+
+<div class="flex" style={`width: ${width}px; height: ${height}px`}>
+    <div class="relative w-full">
+        <div class="absolute">
+            <img src="/exhibition/exhibition.png" alt="exhibition" />
+        </div>
+        <button
+            class={`absolute opacity-${opacity} cursor-none`}
+            style={`left: ${qrX}px; top: ${qrY}px; width: ${qrW}px; height: ${qrH}px;`}
+            on:mouseenter={() => {
+                qrW = width * 0.17;
+                opacity = 1;
+            }}
+            on:mouseleave={() => {
+                qrW = width * 0.045;
+                opacity = 0;
+            }}
+        >
+            <img src="/exhibition/qrcode.png" alt="qrcode" />
+        </button>
+        <div class="opacity-0"></div>
+        <div class="opacity-1"></div>
+    </div>
+</div>
--- src/routes/exhibition/+page.svelte
+++ src/routes/exhibition/+page.svelte
@@ -1,3 +1,23 @@
-<div class="flex flex-col gap-6">
-    <div>EXIBITION</div>
+<script>
+    import ExhibitionPoster from "$lib/components/ExhibitionPoster.svelte";
+    let exhibitionWidth = 0;
+    // $: exhibitionHeight = (exhibitionWidth * 1684) / 1191;
+</script>
+
+<div class="flex flex-col gap-6 mb-40">
+    <div><b>EXIBITION</b></div>
+    <div>
+        <a href="https://www.ischive.com/2024jungmoon24" target="_blank">
+            https://www.ischive.com/2024jungmoon24 ↗
+        </a>
+    </div>
+    <div bind:clientWidth={exhibitionWidth}>
+        <ExhibitionPoster width={exhibitionWidth} />
+    </div>
 </div>
+
+<style>
+    a {
+        color: rgb(59 130 246);
+    }
+</style>
